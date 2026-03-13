83055d134c90eef0d98ab92ae3e4ef5cf04b2291
23537av-cell <23537av@hvhs.school.nz>
2026-03-13 07:37:26 +0000

    falling

--- fallingcandies.js
+++ fallingcandies.js
@@ -1,15 +1,22 @@
 /*******************************************************/
-// P5.play: t22_keyboard
-// Move sprite via keyboard
+// P5.play: Fallingcandy
+// candies fall due to gravity
 // Written by ???
 /*******************************************************/
-	
+console.log("fallingcandies: ");	
 /*******************************************************/
 // setup()
 /*******************************************************/
 function setup() {
 
-	cnv = new Canvas(750, 800);
+	 cnv = new Canvas(700, 850);
+     const Fallingcandy = document.getElementById('falling-candy');
+     let objectPosition = 0;
+     const gravity = 0.5;
+	 
+
+
+	
 	 
 
 	
