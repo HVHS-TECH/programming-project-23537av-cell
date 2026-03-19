414eae7bc021d0d32c530d53e83b30e031985f63
23537av-cell <23537av@hvhs.school.nz>
2026-03-19 23:32:42 +0000

    falling

--- fallingcandies.js
+++ fallingcandies.js
@@ -10,18 +10,18 @@ console.log("fallingcandies: ");
 function setup() {
 
 	 cnv = new Canvas(700, 850);
-     const Fallingcandy = document.getElementById('falling-candy');
-     let objectPosition = 0;
-     const gravity = 0.5;
-	 
+      basket = new Sprite(350, 800, 120, 20, 'k');
+      basket.color = 'blue';
+      score = 10;
 
+      world.gravity.y = 20;
+      candy = new Sprite(50,100, 10, 15, 'd');
+      candy.color = 'red';
+      candy.rotationSpeed = 2; 
 
 	
-	 
-
 	
-	
-	console.log("setup: ");
+	console.log("hello: score"+frameCount);
     
 }
 	
@@ -29,10 +29,18 @@ function setup() {
 // draw()
 /*******************************************************/
 function draw() {
+      // make a new candy sometimes
+  
+    console.log("hello: score"+frameCount);
+    candies = new Sprite(random(50,650), 0, 10, 15, 'd');
+    candies.color = 'yellow';
+    candies.rotationSpeed = 2;
+    basket.x = mouse.x;
+ 
 
 	  
 	  
-	background('lightblue');
+	background('skyblue');
 	
 }
 
