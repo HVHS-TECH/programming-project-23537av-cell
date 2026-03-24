2854e84fe3e28e87afa9ca2b01633ed3aacac317
23537av-cell <23537av@hvhs.school.nz>
2026-03-24 07:43:43 +0000

    falling

--- fallingcandies.js
+++ fallingcandies.js
@@ -2,8 +2,7 @@
 // P5.play: Fallingcandy
 // candies fall due to gravity
 // Written by ???
-/*******************************************************/
-console.log("fallingcandies: ");	
+/*******************************************************/	
 /*******************************************************/
 // setup()
 /*******************************************************/
@@ -12,37 +11,41 @@ function setup() {
 	 cnv = new Canvas(700, 850);
       basket = new Sprite(350, 800, 120, 20, 'k');
       basket.color = 'blue';
-      score = 10;
+      score = 0;
+      candy.gravity.x = 5;
 
-      world.gravity.y = 20;
       candy = new Sprite(50,100, 10, 15, 'd');
       candy.color = 'red';
       candy.rotationSpeed = 2; 
 
-	
-	
-	console.log("hello: score"+frameCount);
-    
-}
+      
+      console.log("hello:score"+ FrameCount) ;
+} 
 	
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
       // make a new candy sometimes
-  
-    console.log("hello: score"+frameCount);
-    candies = new Sprite(random(50,650), 0, 10, 15, 'd');
+    console.log("hello:score"+ FrameCount) ;
+    candies = new Sprite(random(10,650), 0, 10, 15, 'd');
     candies.color = 'yellow';
     candies.rotationSpeed = 2;
     basket.x = mouse.x;
- 
 
-	  
-	  
-	background('skyblue');
+   if (candy.collides(bucket))
+    candy.remove();
+    score++;
+
+    candies = new Sprite(random(10,650), 0, 10, 15, 'd');
+    candies.color = 'yellow';
+    candies.rotationSpeed = 2;
+    basket.x = mouse.x;
+
+
+    }
+    background('skyblue');
 	
-}
 
 /*******************************************************/
 //  END OF APP
